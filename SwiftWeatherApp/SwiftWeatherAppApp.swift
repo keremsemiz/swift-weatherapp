320b3e68488c56ee4abdd4e6fd91dbdde0bdf27a
Kerem Semiz <kerox350@gmail.com>
2024-06-25 18:17:40 +0200

    Initial Commit

---
+++ SwiftWeatherApp/SwiftWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWeatherAppApp.swift
+//  SwiftWeatherApp
+//
+//  Created by Kerem Semiz on 25.06.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
