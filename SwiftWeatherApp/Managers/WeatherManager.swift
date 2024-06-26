6b0e7c9b0abe96d5e0d6cf0f47fc05f4f4ae2b89
Kerem Semiz <kerox350@gmail.com>
2024-06-26 13:42:57 +0200

    do work

--- SwiftWeatherApp/Managers/WeatherManager.swift
+++ SwiftWeatherApp/Managers/WeatherManager.swift
@@ -9,19 +9,30 @@ import Foundation
 import CoreLocation
 
 class WeatherManager {
+    private let apiKey = "8fd3ee16a35404b103a491eef38a549e"
+    
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=8fd3ee16a35404b103a491eef38a549e&units=metric") else { fatalError("Missing URL") }
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric") else { fatalError("Missing URL") }
 
 
         let urlRequest = URLRequest(url: url)
-        
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
-        
         guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error while fetching data") }
-        
         let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
-        
         return decodedData
+    }
+    
+    func getWeatherByCity(city: String) async throws -> ResponseBody {
+        let cityQuery = city.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityQuery)&appid=\(apiKey)&units=metric") else { fatalError("Missing URL") }
+        
+        
+        let urlRequest = URLRequest(url: url)
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error while fetching data") }
+        let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
+        return decodedData
+        
     }
 }
 
